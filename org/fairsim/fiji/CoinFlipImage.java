6d18712ee496a24f04c75562c220be8ed10449f9
Marcel Müller <muellerphysics@gmail.com>
2026-01-20 18:10:06 +0100

    Stack processing added to coinflip algorithm

--- org/fairsim/fiji/CoinFlipImage.java
+++ org/fairsim/fiji/CoinFlipImage.java
@@ -4,10 +4,9 @@ import ij.IJ;
 import ij.plugin.PlugIn;
 import ij.ImagePlus;
 import ij.process.ShortProcessor;
+import ij.ImageStack;
+
 import org.fairsim.utils.SimpleMT;
-
-
-
 
 public class CoinFlipImage implements PlugIn {
 
@@ -43,64 +42,88 @@ public class CoinFlipImage implements PlugIn {
             IJ.showMessage("No image open");
             return;
         }
-    
-        int width = im.getWidth();
-        int height = im.getHeight();
+   
+        final int width = im.getWidth();
+        final int height = im.getHeight();
+        
+        ImageStack stack = im.getStack();
+        ImageStack [] outputStack = new ImageStack[2];
+        for (int i = 0; i < 2; i++) {
+            outputStack[i] = new ImageStack(width, height);
+        }
 
-        ShortProcessor sp = (ShortProcessor) im.getProcessor().convertToShortProcessor();
-        short[] pixels = (short[]) sp.getPixels();
-
-        short [][] imgs = new short[2][];
-        imgs[0] = new short[width * height];
-        imgs[1] = new short[width * height];
-
-        XorShiftState [] prngState = new XorShiftState[width * height];
-        XorShiftState stateInit = new XorShiftState();
-        stateInit.seed = 123456789; // fixed seed for reproducibility
 
         // initialize PRNG states for each pixel
         long start = System.nanoTime();
+        XorShiftState stateInit = new XorShiftState();
+        stateInit.seed = 123456789; // fixed seed for reproducibility
+        XorShiftState [] prngState = new XorShiftState[width * height];
         
         for (int i = 0; i < width * height; i++) {        
             prngState[i] = new XorShiftState();
             xorshift_star(stateInit);
             prngState[i].seed = stateInit.value; // simple seed based on pixel index
         }
+        
         long end = System.nanoTime();
+        
         IJ.log(String.format("PRNG initialization time (ms): %.2f", (end - start) / 1_000_000.0));
 
+        // process each slice   
+        for (int s = 1; s <= stack.getSize(); s++) {
 
-        start = System.nanoTime();
-        new SimpleMT.PFor(0, width * height) {
-            @Override
-            public void at(int i) {
-                int count = pixels[i] & 0xFFFF; // interpret as unsigned short
-                for (int j = 0; j < count; j++) {
-                    xorshift_star(prngState[i]);
-                    if (prngState[i].value < 0) {
-                        imgs[0][i]++;
-                    } else {
-                        imgs[1][i]++;
+            im.setSlice(s);
+            ShortProcessor sp = (ShortProcessor) im.getStack().getProcessor(s).convertToShortProcessor();
+            short[] pixels = (short[]) sp.getPixels();
+
+            short [][] imgs = new short[2][];
+            imgs[0] = new short[width * height];
+            imgs[1] = new short[width * height];
+
+        
+            start = System.nanoTime();
+            new SimpleMT.PFor(0, width * height) {
+                @Override
+                public void at(int i, int threadID) {
+                    if (threadID == 0 && i % width == 0) {
+                        IJ.showProgress(i, width * height);
                     }
                 }
-            }
-        };
-        
-        end = System.nanoTime();
-        IJ.log(String.format("Coin flip processing time (ms): %.2f", (end - start) / 1_000_000.0));
 
-        // create output images
-        ShortProcessor sp1 = new ShortProcessor(width, height);
-        ShortProcessor sp2 = new ShortProcessor(width, height);
-        short[] outPixels1 = (short[]) sp1.getPixels();
-        short[] outPixels2 = (short[]) sp2.getPixels();
-        System.arraycopy(imgs[0], 0, outPixels1, 0, width * height);
-        System.arraycopy(imgs[1], 0, outPixels2, 0, width * height);
+                @Override
+                public void at(int i) {
+                    int count = pixels[i] & 0xFFFF; // interpret as unsigned short
+                    for (int j = 0; j < count; j++) {
+                        xorshift_star(prngState[i]);
+                        if (prngState[i].value < 0) {
+                            imgs[0][i]++;
+                        } else {
+                            imgs[1][i]++;
+                        }
+                    }
+                }
+            };
+            
+            end = System.nanoTime();
+            IJ.log(String.format("Slice %d coin flip processing time (ms): %.2f", s, (end - start) / 1_000_000.0));
 
-        ImagePlus outIm1 = new ImagePlus("Coin Flip Image 1", sp1);
-        ImagePlus outIm2 = new ImagePlus("Coin Flip Image 2", sp2);
+            // create output images
+            ShortProcessor sp1 = new ShortProcessor(width, height);
+            ShortProcessor sp2 = new ShortProcessor(width, height);
+            short[] outPixels1 = (short[]) sp1.getPixels();
+            short[] outPixels2 = (short[]) sp2.getPixels();
+            System.arraycopy(imgs[0], 0, outPixels1, 0, width * height);
+            System.arraycopy(imgs[1], 0, outPixels2, 0, width * height);
+
+            outputStack[0].addSlice(stack.getSliceLabel(s), sp1);
+            outputStack[1].addSlice(stack.getSliceLabel(s), sp2);
+        }
+
+        ImagePlus outIm1 = new ImagePlus(im.getTitle() + "_coinflip_heads", outputStack[0]);
         outIm1.show();
+        ImagePlus outIm2 = new ImagePlus(im.getTitle() + "_coinflip_tails", outputStack[1]);
         outIm2.show();
+
 
 
     }
